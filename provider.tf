eb67f6407b4a6d7a7503469d45064f137ca52da5
KANILWEB <kumaranil.azureweb@gmail.com>
2021-07-13 22:57:19 +0530

    files

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "6719c79f-78bc-43a0-92a1-970682fce113"
+  client_id       = "a14f1456-f9fb-4c20-803d-6088efe78c69"
+  client_secret   = "3z_F.dEh1U9F7V9--.UrXD4IMqXn4LCp8i"
+  tenant_id       = "76a19ab5-afc3-4e40-acdc-4135726790a9"
+}
